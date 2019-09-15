010c62c96ea72bb7411eaabdfc9e5044a61fb8a2
Roberto <robertodermato@gmail.com>
2019-09-15 11:56:54 -0300

    Contratos tel fixo

--- src/ContratoTelFixoResidencial.java
+++ src/ContratoTelFixoResidencial.java
@@ -7,4 +7,18 @@ public class ContratoTelFixoResidencial extends ContratoTelFixo {
     public ContratoTelFixoResidencial(String nome, String CPF, int numero) {
         super(nome, CPF, numero);
     }
+
+    public double getCustoFixo(){
+        return super.getNumeroMinutosParaFixo()*0.1;
+    }
+
+    public double getCustoMovel(){
+        return super.getNumeroMinutosParaMovel()*0.8;
+    }
+
+    @Override
+    public String toString() {
+        return "Contrato Telefone Fixo Residencial: " + super.toString() + " Gasto em ligaçoes para telefone fixo: " + getCustoFixo() +
+                " Gasto em ligações para telefone móvel: " + getCustoMovel();
+    }
 }
